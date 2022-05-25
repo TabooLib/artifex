0ceb0679ca75e354f0d495f97bf0a60ae56a0758
坏黑 <b@skymc.me>
2022-05-26 03:33:30 +0800

    update

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,6 +1,13 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.io.FileInputStream
+import java.util.zip.ZipOutputStream
+import java.io.FileOutputStream
+import java.util.jar.JarEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipInputStream
 
 plugins {
+    java
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -15,11 +22,35 @@ dependencies {
 
 tasks {
     withType<ShadowJar> {
-        archiveBaseName.set("Artifex")
         archiveClassifier.set("")
         exclude("META-INF/maven/**")
         exclude("META-INF/tf/**")
         exclude("module-info.java")
+    }
+    create("collect") {
+        dependsOn(build)
+        doFirst {
+            val version = project.version
+            val file = projectDir.resolve("build/libs/plugin-$version.jar")
+            val newFile = projectDir.resolve("build/libs/plugin-$version-all.jar")
+            ZipFile(file).use { old ->
+                ZipOutputStream(FileOutputStream(newFile)).use { new ->
+                    for (entry in old.entries()) {
+                        new.putNextEntry(entry)
+                        if (!entry.isDirectory) {
+                            new.write(old.getInputStream(entry).readBytes())
+                        }
+                        new.closeEntry()
+                    }
+                    new.putNextEntry(JarEntry("runtime/bridge.jar"))
+                    new.write(rootProject.file("project/common-bridge/build/libs/common-bridge-$version-origin.jar").readBytes())
+                    new.closeEntry()
+                    new.putNextEntry(JarEntry("runtime/core.jar"))
+                    new.write(rootProject.file("project/common-runtime/build/libs/common-runtime-$version.jar").readBytes())
+                    new.closeEntry()
+                }
+            }
+        }
     }
     build {
         dependsOn(shadowJar)
