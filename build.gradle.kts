8b9dd18afb9e898526c6d85098713d4a2de17cd3
Ling_Haner <ling_haner@163.com>
2023-02-02 20:12:06 +0800

    update taboolib

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.gradle.maven-publish")
     id("org.jetbrains.kotlin.jvm") version "1.5.10" apply false
 
-    id("io.izzel.taboolib") version "1.51" apply false
+    id("io.izzel.taboolib") version "1.56" apply false
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
 }
 
--- project/common-bridge/src/main/kotlin/ink/ptms/artifex/bridge/Backend.kt
+++ project/common-bridge/src/main/kotlin/ink/ptms/artifex/bridge/Backend.kt
@@ -2,32 +2,33 @@ package ink.ptms.artifex.bridge
 
 import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.ProxyPlayer
-import taboolib.common.platform.command.CommandBuilder
 import taboolib.common.platform.command.CommandContext
+import taboolib.common.platform.command.component.CommandComponent
+import taboolib.common.platform.command.component.CommandComponentDynamic
 
-fun CommandBuilder.CommandComponent.execute(function: (sender: ProxyCommandSender, args: CommandContext<ProxyCommandSender>) -> Unit) {
+fun CommandComponent.execute(function: (sender: ProxyCommandSender, args: CommandContext<ProxyCommandSender>) -> Unit) {
     execute<ProxyCommandSender> { sender, context, _ -> function(sender, context) }
 }
 
-fun CommandBuilder.CommandComponent.executeAsPlayer(function: (sender: ProxyPlayer, args: CommandContext<ProxyPlayer>) -> Unit) {
+fun CommandComponent.executeAsPlayer(function: (sender: ProxyPlayer, args: CommandContext<ProxyPlayer>) -> Unit) {
     execute<ProxyPlayer> { sender, context, _ -> function(sender, context) }
 }
 
-fun CommandBuilder.CommandComponentDynamic.suggest(uncheck: Boolean = false, function: () -> List<String>?) {
-    return suggestion<ProxyCommandSender>(uncheck) { _, _ -> function() }
-}
+fun CommandComponentDynamic.suggest(uncheck: Boolean = false, function: () -> List<String>?) =
+    suggestion<ProxyCommandSender>(uncheck) { _, _ -> function() }
 
-fun CommandBuilder.CommandComponentDynamic.suggestAsPlayer(uncheck: Boolean = false, function: () -> List<String>?) {
-    return suggestion<ProxyPlayer>(uncheck) { _, _ -> function() }
-}
+fun CommandComponentDynamic.suggestAsPlayer(uncheck: Boolean = false, function: () -> List<String>?) =
+    suggestion<ProxyPlayer>(uncheck) { _, _ -> function() }
 
-fun CommandBuilder.CommandComponentDynamic.restrict(function: (argument: String) -> Boolean) {
-    return restrict<ProxyCommandSender> { _, _, argument -> function(argument) }
-}
 
-fun CommandBuilder.CommandComponentDynamic.restrictAsPlayer(function: (argument: String) -> Boolean) {
-    return restrict<ProxyPlayer> { _, _, argument -> function(argument) }
-}
+fun CommandComponentDynamic.restrict(function: (argument: String) -> Boolean) =
+    restrict<ProxyCommandSender> { _, _, argument -> function(argument) }
+
+
+fun CommandComponentDynamic.restrictAsPlayer(function: (argument: String) -> Boolean) =
+    restrict<ProxyPlayer> { _, _, argument ->
+        function(argument)
+    }
 
 fun CommandContext<*>.argument(): String {
     return argument(0)
